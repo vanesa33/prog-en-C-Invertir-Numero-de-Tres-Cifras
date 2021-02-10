da9653ca52051dac07174b2a36c35faed49fd12b
Vanidad <57409926+vanesa33@users.noreply.github.com>
2021-02-10 16:46:53 -0300

    Invertir Número de Tres Cifras
    Invertir un número ingresado de tres cifras utilizando los operadores aritméticos en consola C#

---
+++ InvertirNumeroTresCifras/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUM, AUX, DEC, UNI, CEN;
+            string linea;
+            Console.WriteLine("INGRESE NÚMERO DE TRES CIFRAS :");
+            linea = Console.ReadLine();
+            NUM = int.Parse(linea);
+            CEN = NUM / 100;
+            NUM = NUM % 100;
+            
+       
+        DEC = NUM / 10;
+            UNI = NUM % 10;
+            AUX = (UNI * 100) + (DEC * 10) + CEN;
+            Console.WriteLine("NÚMERO INVERTIDO ES: " + AUX);
+            Console.WriteLine("Pulse una Tecla:"); Console.ReadLine();
+        }
+    }
+}
